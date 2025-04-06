6274abbdeaab4e636c35bbebf74c3f90dd6ada32
Everton <evertonbueno2014@gmail.com>
2025-04-05 22:23:25 -0300

    feat: Atualização do componente NotFound para melhor experiência do usuário

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
